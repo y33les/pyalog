593363e59ba62a21ce9b3f60cdf41e2396487c12
Phil Yeeles <phil@yeel.es>
2022-04-28 17:23:48 +0100

    initial AST work; added APL*ad ast.operator subclasses; currently halfway through implementing the APL NodeTransformer behaviour for APLDyads

--- parse.py
+++ parse.py
@@ -1,35 +1,51 @@
 import numpy as np
+import operator as op
+import ast
+from astor import to_source
 from sly import Parser
 from lex import APLLex
 from arrayops import isAtomic
 
-class Node:
-    f=None
-    l=None
-    r=None
+def passfunc(x=0,y=0): # FIXME: Replace this with something proper inside APL*ad
+    pass
 
-    def __init__(self,f,l=None,r=None):
-        self.f=f
-        self.l=l
-        self.r=r
+class APLNilad(ast.operator):
+    f = ast.Name(id='passfunc',ctx='load')
 
-    def show(self):
-        if self.l==None:
-            if self.r==None:
-                return(str(self.f))
+    def __init__(self,f):
+        self.f = ast.Name(id=f,ctx='load')
+        super().__init__()
+
+class APLMonad(ast.operator):
+    f = ast.Name(id='passfunc',ctx='load')
+
+    def __init__(self,f):
+        self.f = ast.Name(id=f,ctx='load')
+        super().__init__()
+
+class APLDyad(ast.operator):
+    f = ast.Name(id='passfunc',ctx='load')
+
+    def __init__(self,f):
+        self.f = ast.Name(id=f,ctx='load')
+        super().__init__()
+
+#class _APLTransformer(ast.NodeTransformer):
+class APLTransformer(ast.NodeTransformer):
+    def visit_APLDyad(self,node):
+        self.generic_visit(node)
+        return node
+
+    def visit_BinOp(self,node):
+        self.generic_visit(node)
+        if isinstance(node.op,APLDyad):
+            if isinstance(node.op.f,ast.Name): # Call
+                return(ast.Call(node.op.f,[node.left,node.right]))
+            elif isinstance(node.op.f,ast.Lambda): # Lambda
+                raise Exception("lambdas not yet implemented") # TODO
+            # TODO: dfn?
             else:
-                return("  "+str(self.f)+"  \n"+"   \\ \n"+"    "+self.r.show())
-        else:
-            return("  "+str(self.f)+"  \n"+" / \\ \n"+self.l.show()+"   "+self.r.show())
-
-class Leaf:
-    val=None
-
-    def __init__(self,val):
-        self.val=val
-
-    def show(self):
-        return(str(self.val))
+                raise TypeError("your dyad doesn't have a name or a lambda as its op")
 
 class APLParse(Parser):
     # Get the token list from the lexer (required)
@@ -38,62 +54,52 @@ class APLParse(Parser):
     # Grammar rules and actions
     precedence = (('right',)+tuple(tokens),)
 
-    """
-    @_('expr PLUS expr') # TODO: This may end up needing an APLExpr class, or maybe an AST node
-    def expr(self, p):
-        return p.expr0 + p.expr1
-
-    @_('MINUS expr')
-    def expr(self, p):
-        return -p.expr
-
-    @_('array')
-    def expr(self, p):
-        return p.array
-
-    @_('array COMMA array')
-    def array(self, p):
-        return np.asarray(list(map(lambda x: np.asarray(x),np.asarray([list(p.array0),list(p.array1)],dtype=object))),dtype=object)
-
-    @_('value')
-    def array(self,p):
-        #if isinstance(p.value,np.ndarray):
-        #    return p.value # TODO: Does this need to be enclosed in another APLArray?
-        #else:
-        #    return np.array([p.value],dtype=object)
-        return np.array([p.value],dtype=object)
-
-    @_('LPAREN expr RPAREN')
-    def value(self, p):
-        return np.array(list(p.expr),dtype=object)
-    """
-
-
     @_('node')
     def expr(self,p):
-        p.node.show()
-        return p.node
+        return ast.fix_missing_locations(ast.Expression(p.node)) # TODO: A proper lineno fix would be nice
 
     @_('MINUS node')
     def node(self, p):
-        return Node('m',l=Leaf('X'),r=p.value)
+        return ast.UnaryOp(ast.USub(),p.node)
 
     @_('node PLUS node')
     def node(self, p):
-        return Node('d',l=p.value0,r=p.value1)
+        return ast.BinOp(p.node0,ast.Add(),p.node1)
+
+    @_('node COMMA node')
+    def node(self, p):
+        return ast.BinOp(p.node0,APLDyad('testfunc'),p.node1)
 
     # TODO: nilads
 
+    # TODO: strings
+
+    #@_('node node')
+    #def node(self, p):
+
     @_('NUMBER')
     def node(self, p):
-        return Node(p.NUMBER)
+        return ast.Constant(p.NUMBER)
 
     @_('CHAR')
     def node(self, p):
         if p.CHAR=='q':
             exit(0)
         else:
-            return Node(p.CHAR)
+            return ast.Constant(p.CHAR)
+
+# Test stuff # TODO: remove
+x=ast.Constant(2)
+y=ast.Constant(3)
+a=ast.BinOp(x,ast.Add(),y)
+b=ast.BinOp(x,APLDyad('testfunc'),y)
+l=APLLex()
+p=APLParse()
+t=APLTransformer()
+def testfunc(x,y):
+    return x-y
+def testparse(e):
+    print(to_source(p.parse(l.tokenize(e))))
 
 if __name__ == '__main__':
     l = APLLex()
@@ -103,6 +109,8 @@ if __name__ == '__main__':
         try:
             text = input('apl > ')
             result = p.parse(l.tokenize(text))
-            print(result)
+            print(to_source(result))
+            #print(eval(compile(result, filename="<ast>", mode="eval")))
+            #print(result)
         except EOFError:
             break
